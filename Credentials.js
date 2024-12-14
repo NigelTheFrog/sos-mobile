9cd117fd0cc1f19fd3f6b6316f49ea0c1b25a2fb
NigelKislew <nigel@sutindo.com>
2024-12-14 12:36:35 +0700

    update version label

--- Credentials.js
+++ Credentials.js
@@ -2,5 +2,5 @@ import { createContext } from "react";
 
 export const CredentialContext = createContext({storedCredentials: {}, setStoredCredentials: ()=>{}})
 export const BaseURL = "http://pstjktr.sos.sutindo.net:8056/api";
-export const AppVersion = '2.0.0';
+export const AppVersion = '2.1.0';
 // export const BaseURL = "http://192.168.88.56/sos-dev/public/api";
